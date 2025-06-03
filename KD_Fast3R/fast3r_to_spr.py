0cd48767d3bde96adc8c2c38f03cce7094cf4000
junhihi <127943579+junhihi@users.noreply.github.com>
2025-06-04 02:43:53 +0900

    Update fast3r_to_spr.py
    코드 최적화

--- KD_Fast3R/fast3r_to_spr.py
+++ KD_Fast3R/fast3r_to_spr.py
@@ -1,46 +1,33 @@
 import numpy as np
 import pymeshlab
 
-def spr(coords_np_Vx3, colors_np_Vx3, gt_normals_np_Vx3=None,
-                                      depth=8, simplify_face_num=None):
+
+def spr(coords_np_Vx3, colors_np_Vx3, depth=9):
+
     """
     SPR 기반 메시 재구성 함수 (입출력: numpy)
 
     Args:
         coords_np_Vx3 (np.ndarray): (N, 3) 포인트 클라우드 정점 좌표
         colors_np_Vx3 (np.ndarray): (N, 3) RGB 색상, [0~1] 범위
-        gt_normals_np_Vx3 (np.ndarray or None): (N, 3) 정규벡터, 없으면 자동 추정
         depth (int): Poisson reconstruction 깊이
-        simplify_face_num (int or None): 지정하면 메시 간소화 수행
 
     Returns:
-        vertices (np.ndarray): (M, 3) 메시 정점 좌표
-        faces (np.ndarray): (K, 3) 메시 삼각형 인덱스
-        colors (np.ndarray): (M, 3) 메시 정점 색상 [0~1]
+        vertices (np.ndarray): (N, 3) 메시 정점 좌표
+        colors (np.ndarray): (N, 3) 메시 정점 색상 [0~1]
     """
+
     coords = coords_np_Vx3 # xyz(N,3)
     colors = colors_np_Vx3 # rgb(N,3)
-    normals = gt_normals_np_Vx3 # 입력 정규 벡터
-    use_GT_normals = gt_normals_np_Vx3 is not None # True -> 사용, False -> 사용X
 
     ms = pymeshlab.MeshSet()
     colors_4 = np.concatenate((colors, np.ones((colors.shape[0], 1))), axis=1)  # N,3 -> N,4
 
-    if use_GT_normals:
-        m = pymeshlab.Mesh(vertex_matrix=coords, v_normals_matrix=normals, v_color_matrix=colors_4) # 좌표, 정규벡터, 색상으로 메쉬 생성
-    else:
-        m = pymeshlab.Mesh(vertex_matrix=coords, v_color_matrix=colors_4) # 좌표와 색상만으로 메쉬 객체 생성
+    m = pymeshlab.Mesh(vertex_matrix=coords, v_color_matrix=colors_4) # 좌표와 색상만으로 메쉬 객체 생성
 
     ms.add_mesh(m)
-
-    if not use_GT_normals:
-        ms.apply_filter('compute_normal_for_point_clouds') # 법선 벡터 자동 계산
-
+    ms.apply_filter('compute_normal_for_point_clouds') # 법선 벡터 자동 계산
     ms.apply_filter('generate_surface_reconstruction_screened_poisson', depth=depth) # 포아송 재구성 사용
-
-    if simplify_face_num is not None: # 메쉬 간소화
-        ms.apply_filter('meshing_decimation_quadric_edge_collapse',
-                        targetfacenum=simplify_face_num, preservetopology=True)
 
     vertices = ms.current_mesh().vertex_matrix() # 정점(포인트 클라우드)
     colors = ms.current_mesh().vertex_color_matrix()[:, :3]  # RGBA → RGB
@@ -49,6 +36,20 @@ def spr(coords_np_Vx3, colors_np_Vx3, gt_normals_np_Vx3=None,
 
 # 뜬금없이 지붕이 생기거나 원반형같이 나오는 등의 이상한 포인트 클라우드를 후처리
 def postprocess(xyz, vertices, colors):
+
+    """
+    SPR 기반 메시 재구성 함수 (입출력: numpy)
+
+    Args:
+        xyz (np.ndarray): (N, 3) 원본 포인트 클라우드 정점 위치 좌표
+        vertices (np.ndarray): (N, 3) 생성된 포인트 클라우드 정점 위치 좌표
+        depth (np.ndarray): (N, 3) 생성된 포인트 클라우드 정점 색상 좌표
+
+    Returns:
+        vertices (np.ndarray): (N, 3) 후처리된 포인트 클라우드 정점 위치 좌표
+        colors (np.ndarray): (N, 3) 후처리된 포인트 클라우드 정점 색상 좌표
+    """
+
     # SPR 입력 범위 저장
     x_min, x_max = np.min(xyz[:, 0]), np.max(xyz[:, 0]) # x 범위 설정
     y_min, y_max = np.min(xyz[:, 1]), np.max(xyz[:, 1]) # y 범위 설정
@@ -62,4 +63,5 @@ def postprocess(xyz, vertices, colors):
     )
     vertices = vertices[in_bounds_mask] # 범위 안의 정점만 살림
     colors = colors[in_bounds_mask] # 범위 안의 색만 살림
+
     return vertices, colors
