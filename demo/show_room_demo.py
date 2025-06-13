4a1d79d75823b8bfbc7ef3637258ad7cdbd17485
SuA0409 <sualee020409@gmail.com>
2025-06-13 15:56:15 +0900

    Update and rename show_room_demo.py to demo/show_room_demo.py

--- demo/show_room_demo.py
+++ demo/show_room_demo.py
@@ -9,7 +9,7 @@ model = ShowRoom(viz=show_viz)
 
 # --2. 데이터 로드
  # Input 이미지 폴더의 주소
-folder_path = "Input Your Image Folder"
+folder_path = "demo/data"  # Input Your Name of Image Folder
  # Input 이미지의 개수
 number_of_image = 3
  # 이미지 전처리
