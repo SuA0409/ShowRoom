15a226c79275c6acd7446cee137787760c2efc04
이석현 <gus2243@korea.ac.kr>
2025-06-15 10:45:14 +0900

    Update show_room_demo.py

--- demo/show_room_demo.py
+++ demo/show_room_demo.py
@@ -19,3 +19,5 @@ model.room = batch_images_load(rooms_path=folder_path, batch_size=1, size=512, s
  # fast3r, spr 그리고 viser의 결합
 model.reconstruction()
 model.building_spr()
+
+input("Press Enter to exit...")
